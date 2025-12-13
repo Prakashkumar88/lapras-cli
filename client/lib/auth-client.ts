8e09134f195c0f21a55cdf0634e96ffcd57f7c0c
Prakashkumar88 <prakashkumar88@gmail.com>
2025-12-13 21:57:32 +0530

    device flow + authentication

--- client/lib/auth-client.ts
+++ client/lib/auth-client.ts
@@ -1,10 +1,8 @@
-// import { deviceAuthorizationClient } from "better-auth/plugins"
-import { createAuthClient } from "better-auth/react"
+import { deviceAuthorizationClient } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3005",
-    
-//       plugins: [ 
-//     deviceAuthorizationClient(), 
-//   ], 
-})
+  baseURL: "http://localhost:3005",
+
+  plugins: [deviceAuthorizationClient()],
+});
